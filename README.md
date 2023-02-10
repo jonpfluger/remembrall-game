72fb22fac4938b544b7eea8200e93d0cf3f0a89f
jonpfluger <jonpfluger718@gmail.com>
2023-02-09 20:42:12 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Remembrall
+
+## Description
+
+test
+
+## Screenshot
+
+![Sample Alt Text]()
+
+## Deployed Link
+
+test
---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+const Footer = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  return (
+    <footer className="w-100 mt-auto text-dark p-4">
+      <div className="container text-center mb-5">
+        {location.pathname !== '/' && (
+          <button
+            className="btn btn-dark mb-3"
+            onClick={() => navigate(-1)}
+          >
+            &larr; Go Back
+          </button>
+        )}
+        <h4>&copy; {new Date().getFullYear()} - Tech Friends</h4>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
