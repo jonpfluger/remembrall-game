b4f5c63de7d16d2444f24872e3788a4102ca4611
jonpfluger <jonpfluger718@gmail.com>
2023-02-13 19:47:15 -0600

    added login and signup forms back in

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
 
-import WizardList from '../components/WizardList';
+import CardGrid from '../components/CardGrid'
 
 import { QUERY_WIZARDS } from '../utils/queries';
 
@@ -12,6 +12,7 @@ const Home = () => {
   return (
     <main>
       <h3>Home</h3>
+      <CardGrid />
     </main>
   );
 };
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -41,8 +41,52 @@ const Login = (props) => {
   };
 
   return (
-    <main>
-      <h3>Login</h3>
+    <main className="flex-row justify-center mb-4">
+      <div className="col-12 col-lg-10">
+        <div className="card">
+          <h4 className="card-header bg-dark text-light p-2">Login</h4>
+          <div className="card-body">
+            {data ? (
+              <p>
+                Success! You may now head{' '}
+                <Link to="/">back to the homepage.</Link>
+              </p>
+            ) : (
+              <form onSubmit={handleFormSubmit}>
+                <input
+                  className="form-input"
+                  placeholder="Your email"
+                  name="email"
+                  type="email"
+                  value={formState.email}
+                  onChange={handleChange}
+                />
+                <input
+                  className="form-input"
+                  placeholder="******"
+                  name="password"
+                  type="password"
+                  value={formState.password}
+                  onChange={handleChange}
+                />
+                <button
+                  className="btn btn-block btn-info"
+                  style={{ cursor: 'pointer' }}
+                  type="submit"
+                >
+                  Submit
+                </button>
+              </form>
+            )}
+
+            {error && (
+              <div className="my-3 p-3 bg-danger text-white">
+                {error.message}
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
     </main>
   );
 };
