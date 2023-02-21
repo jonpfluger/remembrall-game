06a16d97efdd76c0ffc2d0a008b9a998d9c919e3
Jason <jajones414@gmail.com>
2023-02-20 23:48:01 -0600

    Added HomePage and Rules components, need heavy styling and updated Home.js.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useQuery } from "@apollo/client";
-
 import { QUERY_ME } from "../utils/queries";
-
+import HomePage from "../components/HomePage";
 const Home = () => {
-
   const { loading, data } = useQuery(QUERY_ME);
   const wizard = data?.me || [];
 
@@ -13,6 +11,7 @@ const Home = () => {
       <h3 className="text-center px-3 py-4 text-white">
         Welcome {wizard.name}
       </h3>
+      <HomePage />
     </main>
   );
 };
