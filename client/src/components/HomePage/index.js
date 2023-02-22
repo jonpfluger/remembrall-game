3da94471b9d032bfccb2b5efc2fe835abf685c9a
Jason <jajones414@gmail.com>
2023-02-21 19:11:19 -0600

    Changed gif and homepage styling.

--- client/src/components/HomePage/index.js
+++ client/src/components/HomePage/index.js
@@ -1,14 +1,14 @@
 import homeCard from "../../img/cardBack.png";
-import remembrall from "../../img/remembrall.gif";
+import remembrall from "../../img/croppedGif.gif";
 import Rules from "../../components/Rules";
 import '../HomePage/homepage.css'
 
 const HomePage = () => {
   return (
-    <main id="homepage" className="flex-row justify-center px-3 py-4 p-lg-5">
-      <div className="flex-row justify-center px-3 py-4 ">
-        <img src={homeCard} className="homeCard" width="300" />
-        <img src={remembrall} className="py-4 my-4" />
+    <main id="homepage" className="flex-row px-3 py-4 p-lg-5 d-flex justify-content-center ">
+      <div className="flex-row justify-center align-items-center px-3 py-4 ">
+        <img src={homeCard} className="homeCard position-relative" width="300" />
+        <img src={remembrall} className="py-4 my-4 " />
         <Rules />
       </div>
     </main>
