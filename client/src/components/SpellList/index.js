c7beb94a761858e5e2b09dce52a8b1f53325965c
JenniferKiesler <111549041+JenniferKiesler@users.noreply.github.com>
2023-02-21 21:57:55 -0600

    adds the new spell cards

--- client/src/components/SpellList/index.js
+++ client/src/components/SpellList/index.js
@@ -6,23 +6,18 @@ const SpellList = ({wizard, usingSpell}) => {
   const revelio = SpellData[1]
 
   return (
-    <div className='row'>
+    <div className='d-flex col-9 col-sm-8 col-md-7 col-lg-6 col-xl-4 mx-auto'>
       {wizard.spells.map((spell) => (
         <div 
           key={spell.spellId} 
-          className="card m-auto" 
-          style={{width: 150 + "px"}} 
+          className="px-2" 
           onClick={() => {
             usingSpell(wizard._id, spell.name) 
           }}
         >
           {spell.name == "Revelio" 
-            ? <img src={revelio.img} alt={spell.id} className="m-auto mt-3" style={{height: 70 + "px", width: 70 + "px"}} /> 
-            : <img src={accio.img} alt={spell.id} className="m-auto mt-3" style={{height: 70 + "px", width: 70 + "px"}} />}
-          <div className='text-center'>
-            <p className='fs-5 mb-0 fw-bold'>{spell.name}</p>
-            <p className='fs-6'>{spell.description}</p>
-          </div>
+            ? <img src={revelio.img} alt={spell.id} className="img-fluid" /> 
+            : <img src={accio.img} alt={spell.id} className="img-fluid" />}
         </div>
       ))}
     </div>
