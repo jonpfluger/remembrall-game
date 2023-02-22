6cfddc66acb6b4dc32f5ffa8ebc1acbca1797b16
Jason <jajones414@gmail.com>
2023-02-21 18:25:00 -0600

    Pulls changes.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,12 +3,8 @@ import { useQuery } from "@apollo/client";
 import { QUERY_ME } from "../utils/queries";
 
 import ParticleHome from "../components/ParticleHome";
-<<<<<<< HEAD
-import HomePage from "../components/HomePage";
-=======
 
 import image from '../img/nogood.png'
->>>>>>> 6288695e4e99fc7ca70b2743c1f7881a8c39415f
 
 
 const Home = () => {
@@ -27,13 +23,6 @@ const Home = () => {
           </img>
         </div>
       </h3>
-<<<<<<< HEAD
-      <h2 className="container-smoke">
-        <p className="smoke-animation">I solemnly swear that I am up to no good</p>
-      </h2>
-      <HomePage />
-=======
->>>>>>> 6288695e4e99fc7ca70b2743c1f7881a8c39415f
     </main>
   );
 };
