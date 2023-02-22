b77441aed0102b5e2c94f22232de5baa0ba4c92c
Jason <jajones414@gmail.com>
2023-02-21 21:58:21 -0600

    Added house cards to home page.

--- client/src/components/HomePage/index.js
+++ client/src/components/HomePage/index.js
@@ -1,4 +1,8 @@
-import homeCard from "../../img/cardBack.png";
+import house1 from "../../img/HouseCard1.png"
+import house2 from "../../img/HouseCard2.png"
+import house3 from "../../img/HouseCard3.png"
+import house4 from "../../img/HouseCard4.png"
+
 import remembrall from "../../img/My project (49).png";
 import Rules from "../../components/Rules";
 import '../HomePage/homepage.css'
@@ -7,12 +11,18 @@ const HomePage = () => {
   return (
     <main id="homepage" className="flex-row d-flex justify-content-center ">
       <div className="flex-row justify-center align-items-center">
-        <img src={homeCard} className="homeCard position-relative" width="200" />
+      <div className="text-center justify-center px-5">
+        <img src={house1} className="homeCard position-relative py-2" width="200" />
+        <img src={house2} className="homeCard position-relative py-2" width="200" />
+        </div>
         <div className="text-center justify-center px-5">
         <img src={remembrall} className=" position-relative" width="250" />
         <Rules />
         </div>
-        <img src={homeCard} className="homeCard position-relative" width="200" />
+        <div className="text-center justify-center px-5">
+        <img src={house3} className="homeCard position-relative py-2" width="200" />
+        <img src={house4} className="homeCard position-relative py-2" width="200" />
+        </div>
       </div>
     </main>
   );
